868f672c6f015855dbd1c0c9a1dfe87cc8274cae
Andrey Borzenko <andrey@Andreys-MacBook-Air.local>
2021-01-19 23:19:30 -0800

    Final recursive solution of PhoneMnemonics

--- PhoneMnemonics.py
+++ PhoneMnemonics.py
@@ -1,16 +1,17 @@
 def phoneNumberMnemonics(phoneNumber):
 	mnem = []
 	helper(phoneNumber, mnem, '')
-    return mnem
+	return mnem
 
 def helper(phoneNumber, mnem, current):
 	if not phoneNumber:
 		mnem.append(current)
 	else:
 		dct = {2: 'abc', 3: 'def', 4: 'ghi', 5: 'jkl', 6: 'mno', 7: 'pqrs', 8: 'tuv', 9: 'wxyz'}
-		for i, number in enumerate(phoneNumber):
-			if number in ['0', '1']:
-				helper(phoneNumber[i+1:], mnem, current + number)
-			else:
-				for letter in dct[int(number)]:
-					helper(phoneNumber[i+1:], mnem, current + letter)
+		if phoneNumber[0] in ['0', '1']:
+			helper(phoneNumber[1:], mnem, current + phoneNumber[0])
+		else:
+			for letter in dct[int(phoneNumber[0])]:
+				helper(phoneNumber[1:], mnem, current + letter)
+
+print(phoneNumberMnemonics('145'))
