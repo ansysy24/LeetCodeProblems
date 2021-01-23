c858fa73a2cce4d5af5c04b044ff09f46c71fe23
Andrey <andreyborzenko1@gmail.com>
2021-01-22 19:19:44 -0800

    Added MirrorBinaryTree

---
+++ MirrorBinaryTree.py
@@ -0,0 +1,16 @@
+def invertBinaryTree(tree):
+    if tree is None:
+        return
+
+
+tree.right, tree.left = tree.left, tree.right
+invertBinaryTree(tree.right)
+invertBinaryTree(tree.left)
+
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
