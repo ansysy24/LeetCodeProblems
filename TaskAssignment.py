1c698086b83a7270c4927f3561783e24b5887783
Andrey <andreyborzenko1@gmail.com>
2021-02-14 18:49:18 -0800

    Added test for TaskAssighnemt

--- TaskAssignment.py
+++ TaskAssignment.py
@@ -1,3 +1,5 @@
 def taskAssignment(k, tasks):
-    tasks = sorted(enumerate(tasks), key=lambda task:task[1])
+    tasks = sorted(enumerate(tasks), key=lambda task: task[1])
     return [[tasks[j][0], tasks[len(tasks)-1-j][0]] for j in range(k)]
+
+print(taskAssignment(7, [2, 1, 3, 4, 5, 13, 12, 9, 11, 10, 6, 7, 14, 8]))
