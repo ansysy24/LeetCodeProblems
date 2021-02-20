672e90cbd82231127601ac082edf556455954424
Andrey <andreyborzenko1@gmail.com>
2021-02-20 14:19:00 -0800

    Added O(n)/O(n) LargestRange

--- LargestRange.py
+++ LargestRange.py
@@ -18,3 +18,38 @@ def largestRange(array):
     if curr_len > mx_range[0]:
         return [curr_start, curr_end]
     return mx_range[1]
+
+
+def largestRange2(array):
+    s = {}
+    max_range = 1
+    result = [array[0], array[0]]
+
+    for num in array:
+        s[num] = True
+    for i, num in enumerate(array):
+        if s[num]:
+            s[num] = False
+            curr_range = 1
+
+            k = 1
+            while num - k in s:
+                s[num - k] = False
+                curr_range += 1
+                k += 1
+
+
+            m = 1
+            while num + m in s:
+                s[num + m] = False
+                curr_range += 1
+                m += 1
+
+            if curr_range > max_range:
+                max_range = curr_range
+                result = [num - k + 1, num + m - 1 ]
+    return result
+
+
+print(largestRange([1,2,3,4,7,8,11,12]))
+print(largestRange2([1,2,3,4,7,8,11,12]))
