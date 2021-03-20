9bbb15000aa56f428c86ea7e93bb2b28b47bb9a3
Andrey Borzenko <andreyborzenko1@gmail.com>
2021-03-19 22:14:12 -0700

    Added Reconstruct BST

---
+++ ReconstructBST.py
@@ -0,0 +1,29 @@
+class BST:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def reconstructBst(values):
+    head = BST(values.pop(0))
+    while values:
+        nxt = values.pop(0)
+        insert_value_into_tree(head, nxt)
+    return head
+
+def insert_value_into_tree(head, value):
+    head_copy = head
+    while True:
+        if value >= head.value:
+            if head.right is None:
+                head.right = BST(value)
+                break
+            head = head.right
+        else:
+            if head.left is None:
+                head.left = BST(value)
+                break
+            head = head.left
+
+print(reconstructBst([10, 4, 2, 1, 5, 17, 19, 18]))
