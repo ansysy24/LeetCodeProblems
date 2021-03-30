06dad6f61009f317333f29274d4873965fecac3e
Andrey Borzenko <andreyborzenko1@gmail.com>
2021-03-29 23:08:51 -0700

    Added LongestPalindromicSubstring optimized

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -2,36 +2,29 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         longest = 1
         result = s[0]
-        odd_pals_i = []
-        even_pals_i = []
+
         for i in range(1, len(s) - 1):
             if s[i - 1] == s[i + 1]:
-                odd_pals_i.append(i)
+                cur_len = 3
+                j = 2
+                while i - j > -1 and i + j < len(s) and s[i - j] == s[i + j]:
+                    cur_len += 2
+                    j += 1
 
-        for pal in odd_pals_i:
-            cur_len = 3
-            j = 2
-            while pal - j > -1 and pal + j < len(s) and s[pal - j] == s[pal + j]:
-                cur_len += 2
-                j += 1
-
-            if cur_len > longest:
-                longest = cur_len
-                result = s[pal - j + 1:pal + j]
+                if cur_len > longest:
+                    longest = cur_len
+                    result = s[i - j + 1:i + j]
 
         for i in range(1, len(s)):
             if s[i - 1] == s[i]:
-                even_pals_i.append(i)
+                cur_len = 2
+                k = 1
+                while i - k - 1 > -1 and i + k < len(s) and s[i - k - 1] == s[i + k]:
+                    cur_len += 2
+                    k += 1
 
-        for pal in even_pals_i:
-            cur_len = 2
-            k = 1
-            while pal - k - 1 > -1 and pal + k < len(s) and s[pal - k - 1] == s[pal + k]:
-                cur_len += 2
-                k += 1
-
-            if cur_len > longest:
-                longest = cur_len
-                result = s[pal - k:pal + k]
+                if cur_len > longest:
+                    longest = cur_len
+                    result = s[i - k:i + k]
 
         return result
