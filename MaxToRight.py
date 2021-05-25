4b67bc717811796a02ebba4f4cb149c49834d4f6
Andrey Borzenko <andreyborzenko1@gmail.com>
2021-05-24 23:59:05 -0700

    Added brute forse for MaxToRight

--- MaxToRight.py
+++ MaxToRight.py
@@ -2,7 +2,7 @@ class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
         answer = [-1 for _ in range(len(arr))]
 
-        for i in range(len(arr)):
-            answer[i] = max(arr[:i] + arr[i + 1:])
+        for i in range(len(arr) - 1):
+            answer[i] = max(arr[i + 1:])
 
         return answer
