fdcf1eaeda80527de46218f7be1c5d674b382014
Andrey Borzenko <andreyborzenko1@gmail.com>
2021-05-25 23:34:09 -0700

    Added SeachInBST

---
+++ SearchInBST.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root is None:
+            return None
+
+        elif root.val == val:
+            return root
+
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+
+        else:
+            return self.searchBST(root.right, val)
