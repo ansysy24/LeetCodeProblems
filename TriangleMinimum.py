3c48bf95274fafb7455e08f1ebbc67c51da0fc41
Andrey Borzenko <andreyborzenko1@gmail.com>
2021-06-10 22:56:46 -0700

    Added TriangleMinimum alternative solution

--- TriangleMinimum.py
+++ TriangleMinimum.py
@@ -1,3 +1,11 @@
+class Solution2:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        triangle.reverse()
+        for i in range(1, len(triangle)):
+            for j in range(len(triangle[i])):
+                triangle[i][j] = triangle[i][j] + min(triangle[i-1][j], triangle[i-1][j+1])
+        return triangle[-1][0]
+
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         if len(triangle) == 1:
