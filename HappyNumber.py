62886430449546dc47ca62403c860e8ea05db6fc
Andrey Borzenko <andreyborzenko1@gmail.com>
2021-07-13 22:59:27 -0700

    Added solution2 for HappyNumber

--- HappyNumber.py
+++ HappyNumber.py
@@ -14,3 +14,22 @@ class Solution:
                 return False
             else:
                 s.add(n)
+
+
+class Solution2:
+    def isHappy(self, n: int) -> bool:
+        previous = {n}
+        sm = 0
+        for num in list(str(n)):
+            sm += int(num) ** 2
+        previous.add(sm)
+
+        while sm != 1:
+            new_sm = 0
+            for num in list(str(sm)):
+                new_sm += int(num) ** 2
+            if new_sm in previous:
+                return False
+            previous.add(new_sm)
+            sm = new_sm
+        return True
