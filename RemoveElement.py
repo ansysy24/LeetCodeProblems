67484552395b0dce0c905214ff7898a053677b15
Andrey <andreyborzenko1@gmail.com>
2021-07-24 13:06:50 -0700

    Added RemoveElement two pointers solutions

--- RemoveElement.py
+++ RemoveElement.py
@@ -6,3 +6,16 @@ class Solution:
                 nums.pop(i)
             else:
                 i += 1
+
+
+class Solution2:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        j = 0
+        while i < len(nums):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+            i += 1
+
+        return j
