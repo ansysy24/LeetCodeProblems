68c19a056ddd79a50d56e990eb78f06e47b9fd61
Andrey Borzenko <andreyborzenko1@gmail.com>
2024-05-05 21:23:55 -0700

    Rearranged by language. Added first js problem

---
+++ JS/ContainsDuplicates.js
@@ -0,0 +1,10 @@
+var containsDuplicate = function(nums) {
+    let map = new Map()
+    for (let num of nums) {
+        if (map.has(num)) {
+            return true
+        }
+        map.set(num, 1)
+    }
+    return false
+};
